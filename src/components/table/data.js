a62735ce95e911fce79bdad77df1a56e78778af2
MrKazio <115210775+Dzban0@users.noreply.github.com>
2024-12-18 11:44:03 +0100

    Update data.js

--- src/components/table/data.js
+++ src/components/table/data.js
@@ -1,17 +1,29 @@
-export const headers = ['Imię', 'Nazwisko', 'Wiek', 'Miasto'];
+export const headers = ['Imię', 'Nazwisko', 'Rok Urodzenia', 'Wiek', 'Miasto'];
+
+// Helper function to calculate age based on the year of birth
+const calculateAge = (yearOfBirth) => new Date().getFullYear() - yearOfBirth;
 
 export const initialData = [
-    ['Piotr', 'Kowalski', 26, 'Kraków'],
-    ['Ewa', 'Nawałek', 30, 'Warszawa'],
-    ['Andrzej', 'Bielak', 25, 'Tarnów'],
-    ['Franciszek', 'Jurczyk', 33, 'Warszawa'],
-    ['Eryk', 'Ziobro', 23, 'Olsztyn'],
-    ['Dariusz', 'Paluch', 31, 'Kraków'],
-    ['Beata', 'Sikora', 25, 'Oświecim'],
-    ['Paweł', 'Kozioł', 24, 'Olsztyn'],
-    ['Natalia', 'Bierut', 21, 'Kraków'],
-    ['Marcin', 'Długas', 21, 'Poznań'],
-    ['Kinga', 'Adamowicz', 24, 'Bytom'],
-    ['Stanisław', 'Skotniczny', 27, 'Kraków'],
-    ['Filip', 'Wedel', 30, 'Katowice']
-];
+    ['Piotr', 'Kowalski', 1997, null, 'Kraków'],
+    ['Ewa', 'Nawałek', 2004, null, 'Warszawa'],
+    ['Andrzej', 'Bielak', 1998, null, 'Tarnów'],
+    ['Franciszek', 'Jurczyk', 1990, null, 'Warszawa'],
+    ['Eryk', 'Ziobro', 2001, null, 'Katowice'],
+    ['Olga', 'Kosner', 2000, null, 'Dąbrowa Górnicza'],
+    ['Dariusz', 'Pawdel', 1997, null, 'Kraków'],
+    ['Beata', 'Sikora', 1988, null, 'Oświecim'],
+    ['Paweł', 'Kozioł', 1999, null, 'Katowice'],
+    ['Natalia', 'Bierut', 2001, null, 'Kraków'],
+    ['Kinga', 'Pilch', 2002, null, 'Kraków'],
+    ['Marcin', 'Długas', 1995, null, 'Poznań'],
+    ['Zeta', 'Kuźniak', 2001, null, 'Katowice'],
+    ['Kinga', 'Adamowicz', 1999, null, 'Bytom'],
+    ['Stanisław', 'Skotniczny', 1996, null, 'Kraków'],
+    ['Filip', 'Wedel', 1993, null, 'Katowice'],
+    ['Anna', 'Nowak', 2000, null, 'Łódź'],
+    ['Robert', 'Lewandowski', 1988, null, 'Warszawa'],
+    ['Julia', 'Kowal', 1995, null, 'Gdańsk'],
+    ['Michał', 'Wiśniewski', 1992, null, 'Wrocław']
+].map(([firstName, lastName, yearOfBirth, age, city]) => [
+    firstName, lastName, yearOfBirth, calculateAge(yearOfBirth), city
+]);
